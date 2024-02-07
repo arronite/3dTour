673d3b23ab1e14671987b9c882b46fa878e59b9a
arronite <ary45hira2i@gmail.com>
2024-02-07 10:46:43 +0330

    added raw

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App {...{tourName:"script_general.js"}}/>
+    <App {...{tourName:"choghazanbil.js"}}/>
   </React.StrictMode>
 );
 
